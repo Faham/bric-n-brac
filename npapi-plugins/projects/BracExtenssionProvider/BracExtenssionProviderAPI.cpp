83120cc936bfb750c6d02df235c99ec4a1ad5b8e
Faham Negini <faham.negini@gmail.com>
2012-10-26 22:25:17 -0700

    url bugfix

--- npapi-plugins/projects/BracExtenssionProvider/BracExtenssionProviderAPI.cpp
+++ npapi-plugins/projects/BracExtenssionProvider/BracExtenssionProviderAPI.cpp
@@ -1,8 +1,5 @@
-/**********************************************************\
 
-  Auto-generated BracExtenssionProviderAPI.cpp
-
-\**********************************************************/
+//==============================================================================
 
 #include "JSObject.h"
 #include "variant_list.h"
@@ -11,31 +8,23 @@
 
 #include "BracExtenssionProviderAPI.h"
 
+//------------------------------------------------------------------------------
+
+#include <DOM.h>
 #include <string>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <DOM.h>
-//#define _X86_	1
-
-#include "DialogManager.h"
-
-#if defined _WIN32
-	#include <windows.h>
-	#include <atlstr.h>
-	#include <stdlib.h>
-#endif
-
-#ifdef __APPLE__
-	#include <unistd.h>
-#endif
-
 #include <fstream>
 #include <sstream>
 #include <ctime>
 
+#include "DialogManager.h"
 #include "jsonxx.h"
 #include "pugixml.h"
+#include "common.h"
+
+//==============================================================================
 
 namespace jsonxx {
 	extern bool parse_string(std::istream& input, std::string* value);
@@ -43,29 +32,14 @@ namespace jsonxx {
 	extern bool match(const char* pattern, std::istream& input);
 }
 
+//==============================================================================
+
 FB::DOM::DocumentPtr document;
 BracExtenssionProviderAPI * brac_extenssion_provider_api = NULL;
 
-#if defined _WIN32
-	#define popen _popen
-	#define pclose _pclose
-#endif
+//==============================================================================
 
-std::string system_call (const std::string & cmd) {
-	FILE* pipe = popen(cmd.c_str(), "r");
-	if (!pipe) return "ERROR";
-	char buffer[128];
-	std::string result = "";
-	while(!feof(pipe)) {
-		if(fgets(buffer, 128, pipe) != NULL)
-			result += buffer;
-	}
-	pclose(pipe);
-	return result;
-}
-
-FB::variant BracExtenssionProviderAPI::systemCall(const FB::variant& msg)
-{
+FB::variant BracExtenssionProviderAPI::systemCall(const FB::variant& msg) {
 	std::string cmd = msg.convert_cast<std::string>();
 	m_syscall_commands.push_back(cmd);
 	std::string res = system_call(cmd);
@@ -73,8 +47,9 @@ FB::variant BracExtenssionProviderAPI::systemCall(const FB::variant& msg)
 	return res;
 }
 
-FB::variant BracExtenssionProviderAPI::getLogMessage(const FB::variant& msg)
-{
+//------------------------------------------------------------------------------
+
+FB::variant BracExtenssionProviderAPI::getLogMessage(const FB::variant& msg) {
 	int max_id = m_log_messages.size() - 1;
 	if (msg.empty() && max_id >= 0)
 		return m_log_messages[max_id];
@@ -87,8 +62,9 @@ FB::variant BracExtenssionProviderAPI::getLogMessage(const FB::variant& msg)
 	return "Invalid index";
 }
 
-FB::variant BracExtenssionProviderAPI::getCalledCommand(const FB::variant& msg)
-{
+//------------------------------------------------------------------------------
+
+FB::variant BracExtenssionProviderAPI::getCalledCommand(const FB::variant& msg) {
 	int max_id = m_syscall_commands.size() - 1;
 	if (msg.empty() && max_id >= 0)
 		return m_syscall_commands[max_id];
@@ -101,8 +77,9 @@ FB::variant BracExtenssionProviderAPI::getCalledCommand(const FB::variant& msg)
 	return "Invalid index";
 }
 
-FB::variant BracExtenssionProviderAPI::getSysCallResult(const FB::variant& msg)
-{
+//------------------------------------------------------------------------------
+
+FB::variant BracExtenssionProviderAPI::getSysCallResult(const FB::variant& msg) {
 	int max_id = m_syscall_results.size() - 1;
 	if (msg.empty() && max_id >= 0)
 		return m_syscall_results[max_id];
@@ -115,31 +92,7 @@ FB::variant BracExtenssionProviderAPI::getSysCallResult(const FB::variant& msg)
 	return "Invalid index";
 }
 
-#if defined _WIN32
-std::wstring s2ws(const std::string& s)
-{
-	int len;
-	int slength = (int)s.length() + 1;
-	len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0); 
-	wchar_t* buf = new wchar_t[len];
-	std::wstring r(buf);
-	delete[] buf;
-	return r;
-}
-#endif
-
-#if defined __APPLE__
-std::string escapepath(const std::string & s)
-{
-	std::string escaped_s = s;
-	int pos = 0;
-	while ((pos = escaped_s.find(" ", pos)) != std::string::npos) {
-		escaped_s.replace(pos, 1, "\\ ");
-		pos += 2;
-	}
-	return escaped_s;
-}
-#endif
+//------------------------------------------------------------------------------
 
 void onReturn(const std::string& path) {
 	BracExtenssionProviderAPI * api_ptr = brac_extenssion_provider_api;
@@ -159,10 +112,10 @@ void onReturn(const std::string& path) {
 	#if defined _WIN32
 		std::string command = "cd /d \"" + root_dir + "\" & bin\\7za.exe e -y \"" + path + "\" brac.xml";
 	#elif defined __APPLE__
-		std::string escaped_root_dir = escapepath(root_dir);
+		std::string escaped_root_dir = escape_path(root_dir);
 		api_ptr->log("brac extension root dir (escaped): " + escaped_root_dir);
 
-		std::string escaped_path = escapepath(path);
+		std::string escaped_path = escape_path(path);
 		api_ptr->log("selected brac dir (escaped): " + escaped_path);
 
 		std::string command = "cd " + escaped_root_dir + "; bin/7za e -y " + escaped_path + " brac.xml";
@@ -193,6 +146,8 @@ void onReturn(const std::string& path) {
 	return;
 }
 
+//------------------------------------------------------------------------------
+
 FB::variant BracExtenssionProviderAPI::selectBracFile(const FB::variant& msg) {
 	document = m_host->getDOMDocument();
 	brac_extenssion_provider_api = this;
@@ -204,15 +159,7 @@ FB::variant BracExtenssionProviderAPI::selectBracFile(const FB::variant& msg) {
 	return "done!";
 }
 
-std::vector<std::string> split(const std::string &s, char delim) {
-	std::vector<std::string> elems;
-	std::stringstream ss(s);
-	std::string item;
-	while(std::getline(ss, item, delim)) {
-		elems.push_back(item);
-	}
-	return elems;
-}
+//------------------------------------------------------------------------------
 
 FB::variant BracExtenssionProviderAPI::saveToBracFile(const FB::variant& msg) {
 	std::string command = "";
@@ -243,8 +190,8 @@ FB::variant BracExtenssionProviderAPI::saveToBracFile(const FB::variant& msg) {
 		_splitpath(path.c_str(), drive, dir, fname, ext);
 		command = "rename \"" + path_orig + "\"  \"" + fname + ext + "\"";
 	#elif defined __APPLE__
-		std::string escaped_path = escapepath(path);
-		std::string escaped_path_orig = escapepath(path_orig);
+		std::string escaped_path = escape_path(path);
+		std::string escaped_path_orig = escape_path(path_orig);
 		command = "mv " + escaped_path_orig + " " + escaped_path;
 	#endif
 	systemCall(command);
@@ -252,7 +199,7 @@ FB::variant BracExtenssionProviderAPI::saveToBracFile(const FB::variant& msg) {
 	#if defined _WIN32
 		command = "cd /d \"" + m_extension_path + "\" & bin\\7za.exe e -y \"" + path + "\" brac.xml";
 	#elif defined __APPLE__
-		std::string escaped_extension_path = escapepath(m_extension_path);
+		std::string escaped_extension_path = escape_path(m_extension_path);
 		command = "cd " + escaped_extension_path + "; bin/7za e -y " + escaped_path + " brac.xml";
 	#endif
 	systemCall(command);
@@ -272,19 +219,19 @@ FB::variant BracExtenssionProviderAPI::saveToBracFile(const FB::variant& msg) {
 
 	int new_brac_num = 1 + last_bric.attribute("id").as_int();
 	pugi::xml_node bric_node = brac_node.insert_child_after("bric", last_bric);
-	bric_node.append_attribute("id").set_value(new_brac_num);
-	bric_node.append_attribute("resolution").set_value(bric_info.get<std::string>("resolution").c_str());
-	bric_node.append_attribute("position").set_value(bric_info.get<std::string>("position").c_str());
-	bric_node.append_attribute("rotate").set_value(bric_info.get<std::string>("rotation").c_str());
-	bric_node.append_attribute("scale").set_value(bric_info.get<std::string>("scale").c_str());
-	bric_node.append_attribute("order").set_value(bric_info.get<std::string>("order").c_str());
-	bric_node.append_attribute("alpha").set_value(bric_info.get<std::string>("alpha").c_str());
-	bric_node.append_attribute("revision").set_value(1);
+	bric_node.append_attribute("id")         .set_value(new_brac_num);
+	bric_node.append_attribute("resolution") .set_value(bric_info.get<std::string>("resolution").c_str());
+	bric_node.append_attribute("position")   .set_value(bric_info.get<std::string>("position").c_str());
+	bric_node.append_attribute("rotate")     .set_value(bric_info.get<std::string>("rotation").c_str());
+	bric_node.append_attribute("scale")      .set_value(bric_info.get<std::string>("scale").c_str());
+	bric_node.append_attribute("order")      .set_value(bric_info.get<std::string>("order").c_str());
+	bric_node.append_attribute("alpha")      .set_value(bric_info.get<std::string>("alpha").c_str());
+	bric_node.append_attribute("revision")   .set_value(1);
 	bric_node.append_child(pugi::node_pcdata).set_value(bric_info.get<std::string>("comment").c_str());
 
-	brac_node.attribute("name").set_value(brac_info.get<std::string>("name").c_str());
+	brac_node.attribute("name")  .set_value(brac_info.get<std::string>("name").c_str());
 	brac_node.attribute("artist").set_value(brac_info.get<std::string>("artist").c_str());
-	brac_node.attribute("tags").set_value(brac_info.get<std::string>("tags").c_str());
+	brac_node.attribute("tags")  .set_value(brac_info.get<std::string>("tags").c_str());
 	
 	pugi::xml_object_range<pugi::xml_node_iterator> children = brac_node.children();
 	for (pugi::xml_node_iterator itr = children.begin(); itr != children.end(); ++itr)
@@ -312,20 +259,20 @@ FB::variant BracExtenssionProviderAPI::saveToBracFile(const FB::variant& msg) {
 	#if defined _WIN32
 		command = "mkdir \"" + new_bric_path + "\"";
 	#elif defined __APPLE__
-		std::string escaped_new_bric_path = escapepath(new_bric_path);
+		std::string escaped_new_bric_path = escape_path(new_bric_path);
 		command = "mkdir -p " + escaped_new_bric_path;
 	#endif
 	systemCall(command);
 
 	pugi::xml_document bric_xml;
 	pugi::xml_node root_node = bric_xml.append_child("bric");
-	root_node.append_attribute("version").set_value("1.0");
-	root_node.append_attribute("title").set_value(bric_info.get<std::string>("title").c_str());
+	root_node.append_attribute("version")     .set_value("1.0");
+	root_node.append_attribute("title")       .set_value(bric_info.get<std::string>("title").c_str());
 	root_node.append_attribute("timeinterval").set_value(bric_info.get<std::string>("timeInterval").c_str());
-	root_node.append_attribute("startdate").set_value(bric_info.get<std::string>("startDate").c_str());
-	root_node.append_attribute("url").set_value(bric_info.get<std::string>("url").c_str());
-	root_node.append_attribute("region").set_value(bric_info.get<std::string>("region").c_str());
-	root_node.append_attribute("tags").set_value(bric_info.get<std::string>("tags").c_str());
+	root_node.append_attribute("startdate")   .set_value(bric_info.get<std::string>("startDate").c_str());
+	root_node.append_attribute("url")         .set_value(bric_info.get<std::string>("url").c_str());
+	root_node.append_attribute("region")      .set_value(bric_info.get<std::string>("region").c_str());
+	root_node.append_attribute("tags")        .set_value(bric_info.get<std::string>("tags").c_str());
 
 	pugi::xml_node snapshot_node = root_node.append_child("snapshot");
 	snapshot_node.append_attribute("revision").set_value("1");
@@ -375,7 +322,7 @@ FB::variant BracExtenssionProviderAPI::saveToBracFile(const FB::variant& msg) {
 			+ " --print-backgrounds=on --javascript=on --plugins=on --js-can-access-clipboard=on"
 			+ " --min-width=" + brac_resolution[0]
 			+ " --min-height=" + brac_resolution[1]
-			+ " --url=" + bric_info.get<std::string>("url")
+			+ " --url=\"" + bric_info.get<std::string>("url") + "\""
 			+ " --out-format=png"
 			+ " --out=\"" + bric_screenshot + "\"";
 		systemCall(command);
@@ -401,10 +348,10 @@ FB::variant BracExtenssionProviderAPI::saveToBracFile(const FB::variant& msg) {
 			+ " --height=" + brac_resolution[1]
 			+ " --dir=" + escaped_new_bric_path
 			+ " --filename=temp"
-			+ " " + bric_info.get<std::string>("url");
+			+ " \"" + bric_info.get<std::string>("url") + "\"";
 		systemCall(command);
 
-		std::string escaped_bric_screenshot = escapepath(bric_screenshot);
+		std::string escaped_bric_screenshot = escape_path(bric_screenshot);
 		command = "mv " + escaped_new_bric_path + "/temp-full.png " + escaped_bric_screenshot;
 		systemCall(command);
 
@@ -429,15 +376,7 @@ FB::variant BracExtenssionProviderAPI::saveToBracFile(const FB::variant& msg) {
 	return "done!";
 }
 
-const char * getEnv( const char * var ) {
-	const char * val = ::getenv(var);
-	if ( val == 0 ) {
-		return "";
-	}
-	else {
-		return val;
-	}
-}
+//------------------------------------------------------------------------------
 
 FB::variant BracExtenssionProviderAPI::setExtensionPath(const FB::variant& msg) {
 	m_extension_path = msg.cast<std::string>();
@@ -452,14 +391,14 @@ FB::variant BracExtenssionProviderAPI::setExtensionPath(const FB::variant& msg) 
 	std::string signedvar = '%' + std::string(env_var) + '%';
 	index = m_extension_path.find(signedvar, index);
 	if (std::string::npos != index) {
-		m_extension_path.replace(index, signedvar.length(), getEnv(env_var));
-		log("Extension path is set to ('" + std::string(getEnv(env_var)) + "'): " + m_extension_path);
+		m_extension_path.replace(index, signedvar.length(), get_env(env_var));
+		log("Extension path is set to ('" + std::string(get_env(env_var)) + "'): " + m_extension_path);
 	} else {	
 		log("No environment variable was found: " + m_extension_path);
 	}
 
 #if defined __APPLE__
-	std::string esc_ext_path = escapepath(m_extension_path);
+	std::string esc_ext_path = escape_path(m_extension_path);
 	systemCall("chmod 700 " + esc_ext_path + "/bin/7za");
 	systemCall("chmod 700 " + esc_ext_path + "/bin/convert");
 	systemCall("chmod 700 " + esc_ext_path + "/bin/webkit2png");
@@ -468,8 +407,9 @@ FB::variant BracExtenssionProviderAPI::setExtensionPath(const FB::variant& msg) 
 	return m_extension_path;
 }
 
-BracExtenssionProviderPtr BracExtenssionProviderAPI::getPlugin()
-{
+//------------------------------------------------------------------------------
+
+BracExtenssionProviderPtr BracExtenssionProviderAPI::getPlugin() {
     BracExtenssionProviderPtr plugin(m_plugin.lock());
     if (!plugin) {
         throw FB::script_error("The plugin is invalid");
@@ -477,7 +417,13 @@ BracExtenssionProviderPtr BracExtenssionProviderAPI::getPlugin()
     return plugin;
 }
 
-std::string BracExtenssionProviderAPI::get_version()
-{
+//------------------------------------------------------------------------------
+
+std::string BracExtenssionProviderAPI::get_version() {
     return FBSTRING_PLUGIN_VERSION;
 }
+
+//------------------------------------------------------------------------------
+
+//==============================================================================
+
