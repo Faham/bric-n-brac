be9f3bf03a5ad0f50e7d33a54bbd84c3f0b3d161
Faham Negini <faham.negini@gmail.com>
2014-01-13 09:52:51 -0600

    added brac-sync setup

---
+++ brac-sync/common.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+#===============================================================================
+
+import os, inspect
+import fnmatch
+import platform
+
+#===============================================================================
+
+def getos():
+	_sys = 'unknown'
+
+	if platform.mac_ver() != ('', ('', '', ''), ''):
+			_sys = 'mac'
+	elif platform.win32_ver() != ('', '', '', ''):
+			_sys = 'win'
+
+	return _sys
+		
+#-------------------------------------------------------------------------------
+
+def indent(elem, level=0):
+	i = "\n" + level*"	"
+	if len(elem):
+		if not elem.text or not elem.text.strip():
+			elem.text = i + "	"
+		if not elem.tail or not elem.tail.strip():
+			elem.tail = i
+		for elem in elem:
+			indent(elem, level+1)
+		if not elem.tail or not elem.tail.strip():
+			elem.tail = i
+	else:
+		if level and (not elem.tail or not elem.tail.strip()):
+			elem.tail = i
+				
+#-------------------------------------------------------------------------------
+
+def locate(pattern, root = os.curdir):
+	'''Locate all files matching supplied filename pattern in and below
+	supplied root directory.'''
+	for path, dirs, files in os.walk(os.path.abspath(root)):
+		for filename in fnmatch.filter(files, pattern):
+			yield os.path.join(path, filename)
+			
+#-------------------------------------------------------------------------------
+
+def subdirs(root = os.curdir):
+	'''Locate all subdirectories in and below supplied root directory.'''
+	for path, dirs, files in os.walk(os.path.abspath(root)):
+		for dir in dirs:
+			yield os.path.join(path, dir)
+	
+
+#-------------------------------------------------------------------------------
+
+def function(i):
+    return inspect.stack()[i+1][3]
+
+#===============================================================================
---
+++ brac-sync/setup.py
@@ -0,0 +1,106 @@
+
+#===============================================================================
+
+import platform
+import sys
+env = platform.system().lower()
+
+#===============================================================================
+
+if env == 'darwin':
+	from setuptools import setup
+elif env == 'windows':
+	from distutils.core import setup
+	import py2exe, sys, os
+else:
+	print 'unknown os!'
+	sys.exit(1)
+	
+#===============================================================================
+
+app                  = ['brac_sync.py']
+optimize             = 2
+# distutil options
+data_files           = []
+pkginfo_name         = 'bracsync'
+pkginfo_version      = '1.0.0'
+pkginfo_url          = ''
+pkginfo_license      = 'GNU General Public License'
+pkginfo_author       = 'Faham Negini'
+pkginfo_author_email = 'fan780@mail.usask.ca'
+
+#===============================================================================
+
+if env == 'darwin':
+	py2app_options = {
+		'argv_emulation': True,
+		'dist_dir'      : 'dist',
+		'iconfile'      : 'icon.icns',
+		'optimize'      : optimize,
+		'qt_plugins'    : 'imageformats'
+	}
+	
+	setup(
+		app            = app,
+		options        = {'py2app': py2app_options},
+		setup_requires = ['py2app'],
+		# distutil options
+		data_files     = data_files,
+		name           = pkginfo_name,
+		version        = pkginfo_version,
+		url            = pkginfo_url,
+		license        = pkginfo_license,
+		author         = pkginfo_author,
+		author_email   = pkginfo_author_email,
+	)
+
+#-------------------------------------------------------------------------------
+
+elif env == 'windows':
+	data_files = [('imageformats', [
+		'imageformats_win/qgif4.dll',
+		'imageformats_win/qico4.dll',
+		'imageformats_win/qjpeg4.dll',
+		'imageformats_win/qmng4.dll',
+		'imageformats_win/qsvg4.dll',
+		'imageformats_win/qtga4.dll',
+		'imageformats_win/qtiff4.dll',
+	])]
+	sys.argv.append('py2exe')
+
+	# system dlls to include in the exe package
+	include_dll = ["msvcp90.dll"]
+
+	origIsSystemDLL = py2exe.build_exe.isSystemDLL
+	def isSystemDLL(pathname):
+			if os.path.basename(pathname).lower() in include_dll:
+					return 0
+			return origIsSystemDLL(pathname)
+	py2exe.build_exe.isSystemDLL = isSystemDLL
+
+	#------------------------------
+	
+	py2exe_options = {
+		'optimize'    : optimize,
+		'bundle_files': 3,
+		'compressed'  : True,
+		'includes'    : ['sip',
+						'PyQt4.QtNetwork'],
+	}
+	
+	setup(
+		options      = {'py2exe': py2exe_options },
+		console      = app,
+		zipfile      = None,
+		# distutil options
+		data_files   = data_files,
+		name         = pkginfo_name,
+		version      = pkginfo_version,
+		url          = pkginfo_url,
+		license      = pkginfo_license,
+		author       = pkginfo_author,
+		author_email = pkginfo_author_email,
+		py_modules   = ['common', 'entryutils', 'webkitrenderer'],
+	)
+	
+#===============================================================================
